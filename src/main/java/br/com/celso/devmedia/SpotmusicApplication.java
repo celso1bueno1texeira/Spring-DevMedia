45022983925e0d8ba3f13364155e475145ea9fac
Celso Bueno <cbtbueno@gmail.com>
2020-02-15 16:56:21 -0300

    Home, SpringMVC e Application

---
+++ src/main/java/br/com/celso/devmedia/SpotmusicApplication.java
@@ -0,0 +1,14 @@
+package br.com.celso.devmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpotmusicApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpotmusicApplication.class, args);
+    }
+}
